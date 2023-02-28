76dbbb965d74f3d15d4c3f3c7dfb7ad5c49e8c6e
Egor <63363889+Egorosa@users.noreply.github.com>
2023-02-28 22:39:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,105 @@
+<?php
+// подключаемся к базе данных
+$server = "localhost";
+$login = "root";
+$pass = "";
+$name_db = "rest_api";
 
-<?php 
-  include('validation_cards.php'); 
-?>
+$link = mysqli_connect($server, $login, $pass, $name_db);
 
+if (!$link) {
+    echo "Could not connect to MySQL: " . mysqli_connect_error();
+} 
 
-<html>
-<head>
-    <link rel="stylesheet" href="style.css">
-  <title>Card Validation</title>
-</head>
-<body>
-  <form method="post">
-    <input type="text" name="cardNumber" autofocus>
-    <button type="submit">Validate</button>
-  </form>
-  <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['cardNumber'])) {
-      $cardValidation = new CardValidation();
-      $result = $cardValidation->validate($_POST['cardNumber']);
-      
-    //   echo 'The card is ' . $result[0] . ' and it is ' . $result[1];
+// обработка GET-запроса на получение коллекции ресурсов
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $resource = $_GET['resource'] ?? '';
+
+    switch ($resource) {
+        case 'name':
+            $stmt = mysqli_query($link, "
+                SELECT c.id, c.name
+                FROM city c
+            ");
+            $names = mysqli_fetch_all($stmt, MYSQLI_ASSOC);
+            if ($stmt === false) {
+    die('Ошибка выполнения запроса: ' . mysqli_error($link));
+}
+            $response = ['names' => $names];
+            break;
+        default:
+            $response = ['error' => 'Invalid resource'];
+            break;
     }
-  ?>
-  <p class="result">The card is <?php echo $result[0]; ?> and it is <span><?php echo $result[1]; ?></span></p>
-  
-</body>
-</html>
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+// обработка POST-запроса на создание/изменение ресурса
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $resource = $_GET['resource'] ?? '';
+
+    switch ($resource) {
+        case 'name':
+            $data = json_decode(file_get_contents('php://input'), true);
+            $stmt = mysqli_prepare($link, "
+                INSERT INTO city (name)
+                VALUES (?)
+            ");
+            mysqli_stmt_bind_param($stmt, 's', $data['name']);
+            mysqli_stmt_execute($stmt);
+            $response = ['id' => mysqli_insert_id($link)];
+            break;
+
+        case 'city':
+            $data = json_decode(file_get_contents('php://input'), true);
+            $id = $_GET['id'] ?? '';
+            $stmt = mysqli_prepare($link, "
+                UPDATE city
+                SET name = ?
+                WHERE id = ?
+            ");
+            mysqli_stmt_bind_param($stmt, 'si', $data['name'], $id);
+            mysqli_stmt_execute($stmt);
+            break;
+
+        default:
+            $response = ['error' => 'Invalid resource'];
+            break;
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+// обработка PUT-запроса на обновление ресурса
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'PUT') {
+    $resource = $_GET['resource'] ?? '';
+    $id = $_GET['id'] ?? '';
+
+    switch ($resource) {
+        case 'name':
+            $data = json_decode(file_get_contents('php://input'), true);
+            $stmt = mysqli_prepare($link, "
+                UPDATE city
+                SET name = ?
+                WHERE id = ?
+            ");
+            mysqli_stmt_bind_param($stmt, 'si', $data['name'], $id);
+            mysqli_stmt_execute($stmt);
+            break;
+
+        case 'city':
+            $data = json_decode(file_get_contents('php://input'), true);
+            $stmt = mysqli_prepare($link, "
+                UPDATE city
+                SET name = ?
+                WHERE id = ?
+            ");
+            mysqli_stmt_bind_param($stmt, 'si', $data['name'], $id);
+            mysqli_stmt_execute($stmt);
+            break;
+    }
+}
